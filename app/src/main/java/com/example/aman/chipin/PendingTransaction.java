0e7736fd459ea5d1b8affa4b13c2416ca88fbd1c
abcf98 <amanframewala@gmail.com>
2017-10-27 22:45:29 +0530

    All Pages Added

---
+++ app/src/main/java/com/example/aman/chipin/PendingTransaction.java
@@ -0,0 +1,13 @@
+package com.example.aman.chipin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PendingTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_transaction);
+    }
+}
